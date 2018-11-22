7e7de72c762ce7b130dd88d24b524abcc0dcc02d
unknown <joelapd@gmail.com>
2018-11-22 14:02:38 -0700

    Primer comnit

--- js/autentificacion.js
+++ js/autentificacion.js
@@ -3,7 +3,6 @@ firebase.auth().onAuthStateChanged(function(user) {
     document.getElementById("user_div").style.display="block";
     document.getElementById("log_div").style.display="none";
    
-   
   }
   else {
     // No user is signed in.
@@ -12,21 +11,21 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });         
   //https://firebasestorage.googleapis.com/v0/b/unidrive-027.appspot.com/o/images%2FnameImage19112018_185479.jpg?alt=media&token=67c2159e-b720-47c8-a254-b69dd2f8d064
-  https://firebasestorage.googleapis.com/v0/b/unidrive-027.appspot.com/o/images%2FnameImage20112018_110940.jpg?alt=media&token=e4642cff-e118-4e70-abf5-ac0d64b629bd
+  //firebasestorage.googleapis.com/v0/b/unidrive-027.appspot.com/o/images%2FnameImage20112018_110940.jpg?alt=media&token=e4642cff-e118-4e70-abf5-ac0d64b629bd
 function login(){
 
   var usuario=document.getElementById("usuario").value;
   var contraseña =document.getElementById("contra").value;
 
-  firebase.a)uth(.signInWithEmailAndPassword(usuario, contraseña).catch(function(error) {
+  firebase.auth().signInWithEmailAndPassword(usuario, contraseña).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message; 
-  // ...
+
   window.alert("Try again " +errorMessage);
 });
-
 }
+
  function logout(){
   firebase.auth().signOut();
 }
