286cef63398d2b8201edf17559461cc3bade3097
unknown <joelapd@gmail.com>
2018-12-03 14:17:39 -0700

    uniDrive terminado

--- js/autentificacion.js
+++ js/autentificacion.js
@@ -45,14 +45,14 @@ function login(){
 // cerrar secion 
 
 function logout(){
-  firebase.auth().signOut();
+  firebase.auth().signOut();  
 }
 
 // Elimina un foto de la BD
- var borrar = document.getElementById('borrar')
+var borrar = document.getElementById('borrar')
 function  delOnClick(){
   
-var eventContactsRef = firebase.database().ref('Photos');
+var eventContactsRef = firebase.database().ref('Photos/');
 var query = eventContactsRef.orderByChild('ID').equalTo(eventContactId);
 query.on('child_added', function(snapshot) {
     snapshot.ref.remove();
@@ -60,12 +60,9 @@ query.on('child_added', function(snapshot) {
 }
 
 $('#borrar').click(function(){
-  //var email = document.getElementById('firstname');
-//firebase.database().ref("Photos/-LRzaGEbnJsbwLMhiHtT").remove(); // con esta linea puedo borrar
 firebase.database().ref("Photos/"+texto.value).remove();
-location.reload();// recarga la pagina 
+location.reload(); // recarga la pagina 
 });
-
 
 // aqui estoy leyendo de la BD 
 //me da toda la base de datos de la tabla Photos
@@ -73,18 +70,24 @@ firebase.database().ref("Photos").on("child_added", function(s) {
   var user = s.val();
 
   var imageRef = firebase.database().ref("Photos/" + user.ID);
+  var photoKey = firebase.database().ref("Photos/" + user.ID).key;
+
   imageRef.once('value').then(function(snapshot) {
       var photoDiv = document.createElement("div");
-      var btnDelete = '<input type="submit" value="Borrar" class="btn btn-danger" name="Borrar"/>';
+      //var btnDelete = '<input type="button" value="Borrar" id="btn btn-danger" name="Borrar" onclick="deletePhoto()"/>';
 
       var photo = document.createElement($('#photo').append("<img width='350' src='" + user.downloadUrl + "'/> <br/>" + 
                                           "Nombre: " + user.name + "<br/>" + "Descripción: "+ user.description + "<br/>" + 
                                           "Latitud: " + user.latitude + "<br/>" + "longitud: " + user.longitude + "<br/>" + 
-                                          "ID: " + user.ID + "<br/>" + btnDelete + "<br/> <br/>"));
+                                          "ID: " + user.ID + /*"<br/>" + btnDelete +*/ "<br/> <br/>"));
                                           
       photoDiv.appendChild(photo);
       var currentDiv = document.getElementById("photos");
       document.body.insertBefore(photoDiv, currentDiv);
       var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+
+
+
     });
   });
+
